ba99085e49ad50b77c1c963ff310e5631c3fcb6a
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-09-08 18:30:42 +0200

    add-include

--- index.php
+++ index.php
@@ -1,35 +1,37 @@
 <?php
-
-    function randomPassword(){
-
-        //array di lettere con cui creare la password
-        $elements_array = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*_';
-
-        //numero ottenuto dal get
-        $user_number = $_GET["number"] ?? null;
-
-        //condizione che permette di avere una password minima di 1 carattere
-        if ($user_number < 0){
-            $user_number = 1;
-        };
-
-        $password = [];        
-            //ciclo per la creazione della password
-        for ($i=0; $i < $user_number; $i++) { 
-            $num = rand(0, strlen($elements_array) - 1);
-            $password[] = $elements_array[$num];
-        };
+    include_once "./function.php";
 
 
-        if ($password){
-            $final_password =implode('',$password);
-        } else {
-            $final_password = "La tua password non è stata ancora creata";
-        }
+    // function randomPassword(){
+
+    //     //array di lettere con cui creare la password
+    //     $elements_array = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*_';
+
+    //     //numero ottenuto dal get
+    //     $user_number = $_GET["number"] ?? null;
+
+    //     //condizione che permette di avere una password minima di 1 carattere
+    //     if ($user_number < 0){
+    //         $user_number = 1;
+    //     };
+
+    //     $password = [];        
+    //         //ciclo per la creazione della password
+    //     for ($i=0; $i < $user_number; $i++) { 
+    //         $num = rand(0, strlen($elements_array) - 1);
+    //         $password[] = $elements_array[$num];
+    //     };
+
+
+    //     if ($password){
+    //         $final_password =implode('',$password);
+    //     } else {
+    //         $final_password = "La tua password non è stata ancora creata";
+    //     }
     
-        //return + implode per creare la parola
-        return $final_password;
-    };    
+    //     //return + implode per creare la parola
+    //     return $final_password;
+    // };    
     
 ?>
 
